ccb2ac3d9c732539f7b87ce689bacfcb07d7b639
Alessandro Pozone <alessandro.pozone@hotmail.com>
2020-07-10 19:17:13 +0200

    Add project files.

---
+++ Poz1.LogicProver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Poz1.LogicProver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
